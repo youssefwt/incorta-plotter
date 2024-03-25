6b8728919510c867e98fe45ac7fd2ccd2d20d447
youssefw <ywtalaat@gmail.com>
2024-03-25 14:09:30 +0200

    adding tool tips

--- src/features/side-menu/SideMenu.tsx
+++ src/features/side-menu/SideMenu.tsx
@@ -1,14 +1,32 @@
 import { Dimension, Measure, useColumns } from "@/api/columns";
 import { Separator } from "components/ui/separator";
 import ColumnItem from "./ColumnItem";
-
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Info } from "lucide-react";
 const SideMenu = () => {
   const { data, isLoading, isFetching } = useColumns();
   const loading = isLoading || isFetching;
   return (
     <div className="space-y-4">
       <div className="space-y-1">
-        <h4 className="text-lg underline">Dimensions</h4>
+        <div className="flex items-center gap-5">
+          <h4 className="text-lg underline">Dimensions</h4>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Info size={20} color="grey" />
+              </TooltipTrigger>
+              <TooltipContent align="start">
+                <p>Drag one dimension</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
         {!!loading &&
           !data &&
           [1, 2, 3].map((el) => (
@@ -21,7 +39,19 @@ const SideMenu = () => {
       </div>
       <Separator />
       <div className="space-y-1">
-        <h4 className="text-lg underline">Measures</h4>
+        <div className="flex items-center gap-5">
+          <h4 className="text-lg underline">Measures</h4>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Info size={20} color="grey" />
+              </TooltipTrigger>
+              <TooltipContent align="start">
+                <p>Drag one or more measures</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
         {!!loading &&
           !data &&
           [4, 5, 6].map((el) => (
